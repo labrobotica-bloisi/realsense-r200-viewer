9db7db5387a892da6a939919f93c9cf601e20ba4
dbloisi <bloisi@dis.uniroma1.it>
2018-01-22 19:09:31 +0100

    colormap

--- src/r200_viewer.cpp
+++ src/r200_viewer.cpp
@@ -1,49 +1,98 @@
-#include <ros/ros.h>
+
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <boost/thread/thread.hpp>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
+using namespace sensor_msgs;
+using namespace message_filters;
 
 using namespace std;
 
-void rgb_callback(const sensor_msgs::ImageConstPtr& msg){
-    cv_bridge::CvImagePtr cv_ptr;
+cv::Mat getColorDepth(const cv::Mat& depthImage) {
 
-    try{
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    } catch (cv_bridge::Exception& e){
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
+  cv::Mat colorMat(depthImage.rows, depthImage.cols, CV_8UC3);
+  colorMat.setTo(cv::Scalar(0, 0, 0));
+
+  for(int y = 0; y < depthImage.rows; y++) {
+    for(int x = 0; x < depthImage.cols; x++) {      
+      float d = depthImage.at<float>(y, x);
+      if(d != 0 && d < 4000.0f) {
+	if(d < 500.f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(250, 0, 0);
+        }
+        else if(d < 750.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(200, 50, 50);
+        }
+        else if(d < 1000.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(150, 100, 100);
+        }
+        else if(d < 1250.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(100, 150, 150);
+        }
+        else if(d < 1500.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(50, 200, 200);
+        }
+        else if(d < 1750.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(0, 250, 250);
+        }
+        else if(d < 2000.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(250, 0, 250);
+        }
+        else if(d < 2250.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(250, 50, 200);
+        }
+        else if(d < 2500.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(200, 100, 150);
+        }
+        else if(d < 2750.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(150, 150, 100);
+        }
+        else if(d < 3000.0f) {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(100, 100, 50);
+        }
+        else {
+          colorMat.at<cv::Vec3b>(y,x) = cv::Vec3b(50, 50, 0);
+        }
+      }
     }
+  }
+  return colorMat;
 
-    cv::imshow("RGB", cv_ptr->image);
-    cv::waitKey(30);
 }
 
-
-void depth_callback(const sensor_msgs::ImageConstPtr& msg){
-    cv_bridge::CvImagePtr cv_ptr;
-
+void callback(const ImageConstPtr& depthImage_, const ImageConstPtr& rgbImage_)
+{
+    cv_bridge::CvImagePtr rgbImage;
     try {
-        cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+        rgbImage = cv_bridge::toCvCopy(rgbImage_, sensor_msgs::image_encodings::BGR8);
     } catch (cv_bridge::Exception& e) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
 
-    cv::Mat depthMat = cv_ptr->image.clone();
-
-    double dmin, dmax;
-    cv::minMaxIdx(depthMat, &dmin, &dmax);
-    cv::Mat adjMat;
-    cv::convertScaleAbs(depthMat, adjMat, 255 / dmax);
-
-    cv::Mat colorMat;
-    cv::applyColorMap(adjMat, colorMat, cv::COLORMAP_JET);
-    cv::imshow("Depth", colorMat);
+    cv_bridge::CvImageConstPtr depthImage;
+    try {
+        depthImage = cv_bridge::toCvShare(depthImage_, sensor_msgs::image_encodings::TYPE_32FC1);
+    } catch (cv_bridge::Exception& e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    
+    cv::Mat colorMat = getColorDepth(depthImage->image);
+     
+    cv::imshow("Color Depth", colorMat);
+    cv::imshow("Depth", depthImage->image);
 
     cv::waitKey(30);
 
@@ -54,8 +103,6 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "realsense_r200_viewer");
 
-    ros::NodeHandle nh;
-
     string topic_rgb = "camera/rgb/image_rect_color";
     string topic_depth = "camera/depth_registered/sw_registered/image_rect_raw";
 	
@@ -63,8 +110,13 @@ int main(int argc, char **argv)
     cout << "  - RGB topic: " << topic_rgb << endl;
     cout << "  - Depth topic: " << topic_depth << endl;
 
-    ros::Subscriber rgb_sub = nh.subscribe(topic_rgb, 1, &rgb_callback);
-    ros::Subscriber depth_sub = nh.subscribe(topic_depth, 1, &depth_callback);
+    ros::NodeHandle nh;
+    message_filters::Subscriber<Image> depth_sub(nh, topic_depth, 1);
+    message_filters::Subscriber<Image> rgb_sub(nh, topic_rgb, 1);
+
+    typedef sync_policies::ApproximateTime<Image, Image> syncPolicy;
+    Synchronizer<syncPolicy> sync(syncPolicy(10), depth_sub, rgb_sub);
+    sync.registerCallback(boost::bind(&callback, _1, _2));
 
     ros::spin();
 
